ce057fb6f516681eb94cd832e6fa8efaff64d9d5
Mikhail Kovalenko <mikhail.kovalenko96@gmail.com>
2025-02-08 19:57:36 +0300

    logo update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -286,8 +286,8 @@ function Layout() {
                         {src: '/src/assets/logos/RTUMIREA.svg', alt: 'RTU MIREA'},
                         {src: '/src/assets/logos/Logo.svg', alt: 'ИГРОВАР'},
                         {src: '/src/assets/logos/VKPlay.svg', alt: 'VK Play'},
-                        {src: '/src/assets/logos/PROTOTYPE.svg', alt: 'PROTO TYPE'},
-                        {src: '/src/assets/logos/APRIORI.svg', alt: 'APRI ORI'},
+                        {src: '/src/assets/logos/PROTOTYPE.png', alt: 'PROTO TYPE'},
+                        {src: '/src/assets/logos/APRIORI.png', alt: 'APRI ORI'},
                     ]).flat().map((item, index) => (
                         <a href="#" key={index} className={style.icon}>
                             <img src={new URL(item.src, import.meta.url).href} alt={item.alt}/>
