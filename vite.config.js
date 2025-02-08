8afc96f336fd2acd690b3e48f8e57e77fe85fd7c
Mikhail Kovalenko <mikhail.kovalenko96@gmail.com>
2025-02-08 16:12:55 +0300

    all allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['igrovar.ru', 'игровар.рф', 'igrovar.online', '51.250.100.194'],
+    allowedHosts: ['.'],
   },
 })
